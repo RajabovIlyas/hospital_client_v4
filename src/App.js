823c1a8cb3a24640796f4a18969961224ae72558
RajabovIlyas <ilyasrajabow@gmail.com>
2021-04-16 12:14:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar'
+import Patient from "./components/Content/Patient/Patient";
+import Doctor from "./components/Content/Doctor/Doctor";
+import DentalTechnician from "./components/Content/DentalTechnician/DentalTechnician";
+
+const App = () => {
+    return (
+        <Router>
+            <h3>Проверка обновлений</h3>
+            <Navbar/>
+            <div style={{padding: '30px'}}>
+                <Switch>
+                    <Route path='/dental_technician' component={DentalTechnician}/>
+                    <Route path='/doctor' component={Doctor}/>
+                    <Route path='/patient' component={Patient}/>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/api/api.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const API= axios.create({
+    baseURL: "http://localhost:5000/api",
+    responseType: "json"
+});
+
+export const patientAPI={
+    async getAll() {
+        return await API.get(`patient/`, )
+            .then(res => res.data)
+    },
+    async create(data) {
+        return await API.post(`patient/`, {...data})
+            .then(res => res.data)
+    },
+}
+
+export const doctorAPI={
+    async getAll() {
+        return await API.get(`doctor/`, )
+            .then(res => res.data)
+    },
+    async create(data) {
+        return await API.post(`doctor/`, {...data})
+            .then(res => res.data)
+    },
+}
+
+export const dentalTechnicianAPI={
+    async getAll() {
+        return await API.get(`dental_technician/`, )
+            .then(res => res.data)
+    },
+    async create(data) {
+        return await API.post(`dental_technician/`, {...data})
+            .then(res => res.data)
+    },
+}
